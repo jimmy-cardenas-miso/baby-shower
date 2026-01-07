bfc50e37ed720d9fb034aee86c63140ecd2a319a
jimmy-cardenas-miso <ja.cardenas3388@uniandes.edu.co>
2026-01-07 15:37:47 -0500

    Static render

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static', // Ensure static site generation (SSG)
   integrations: [react(), tailwind()],
+  build: {
+    inlineStylesheets: 'auto', // Optimize CSS loading
+  },
 });
 
